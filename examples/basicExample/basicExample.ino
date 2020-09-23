28621bea51f31f3dce6c5aea4ba3f19616cd7d8a
Nishanth Samala <nishanth.samala@gmail.com>
2020-09-23 15:46:39 -0400

    updated example

--- examples/basicExample/basicExample.ino
+++ examples/basicExample/basicExample.ino
@@ -12,7 +12,7 @@ const int CLK_PIN = 23;
 AddressableLED *leds;
 
 // SmartLed -> RMT driver (WS2812/WS2812B/SK6812/WS2813)
-leds = new OneWireLED(LED_WS2812B, CHANNEL, DATA_PIN, LED_COUNT);
+leds = new OneWireLED(WS2812B, CHANNEL, DATA_PIN, LED_COUNT);
 
 // APA102 -> SPI driver
 // leds = new TwoWireLED(HSPI_HOST, LED_COUNT, CLK_PIN, DATA_PIN)
@@ -23,31 +23,31 @@ void setup() {
 
 uint8_t hue;
 void showGradient() {
-    hue++;
-    // Use HSV to create nice gradient
-    for ( int i = 0; i != LED_COUNT; i++ )
-        (*leds)[i] = Hsv{ static_cast<uint8_t>(hue + 30 * i), 255, 255 };
-    leds->show();
-    // Show is asynchronous; if we need to wait for the end of transmission,
-    // we can use leds.wait(); however we use double buffered mode, so we
-    // can start drawing right after showing.
+  hue++;
+  // Use HSV to create nice gradient
+  for ( int i = 0; i != LED_COUNT; i++ )
+    (*leds)[i] = Hsv{ static_cast<uint8_t>(hue + 30 * i), 255, 255 };
+  leds->show();
+  // Show is asynchronous; if we need to wait for the end of transmission,
+  // we can use leds.wait(); however we use double buffered mode, so we
+  // can start drawing right after showing.
 }
 
 void showRgb() {
-    (*leds)[0] = Rgb{ 255, 0, 0 };
-    (*leds)[1] = Rgb{ 0, 255, 0 };
-    (*leds)[2] = Rgb{ 0, 0, 255 };
-    (*leds)[3] = Rgb{ 0, 0, 0 };
-    (*leds)[4] = Rgb{ 255, 255, 255 };
-    leds->show();
+  (*leds)[0] = Rgb{ 255, 0, 0 };
+  (*leds)[1] = Rgb{ 0, 255, 0 };
+  (*leds)[2] = Rgb{ 0, 0, 255 };
+  (*leds)[3] = Rgb{ 0, 0, 0 };
+  (*leds)[4] = Rgb{ 255, 255, 255 };
+  leds->show();
 }
 
 void loop() {
-    Serial.println("New loop");
-    
-    if ( millis() % 10000 < 5000 )
-        showGradient();
-    else
-        showRgb();
-    delay(50);
+  Serial.println("New loop");
+  
+  if (millis() % 10000 < 5000)
+    showGradient();
+  else
+    showRgb();
+  delay(50);
 }
